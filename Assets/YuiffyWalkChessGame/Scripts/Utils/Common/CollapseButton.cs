3bb0c1d6126921771a41d8f0d9759418ca985440
yuiffy <yuiffy@qq.com>
2019-02-02 02:12:17 +0800

    make a shop ui

---
+++ Assets/YuiffyWalkChessGame/Scripts/Utils/Common/CollapseButton.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CollapseButton : MonoBehaviour
+{
+    Transform innerWindow;
+    public bool isOpen = false;
+    // Start is called before the first frame update
+    void Start()
+    {
+        innerWindow = transform.Find("InnerWindow");
+        SetOpen(isOpen);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public void SetOpen(bool _isOpen) {
+        isOpen = _isOpen;
+        innerWindow.gameObject.SetActive(isOpen);
+    }
+
+    public void switchOpen(){
+        isOpen = !isOpen;
+        innerWindow.gameObject.SetActive(isOpen);
+    }
+}
